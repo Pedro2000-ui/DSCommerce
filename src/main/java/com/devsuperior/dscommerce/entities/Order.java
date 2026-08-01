1252565cc77a3d7e28f81c8d93efa6f037de488a
Pedro Henrique <pholiveirabrito@hotmail.com>
2026-08-01 11:11:50 -0300

    feat: Payment e relacionamento um-para-um

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -20,12 +20,18 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User        client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+
     public Order() {}
 
-    public Order(Long id, Instant moment, OrderStatus status) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
+        this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -36,11 +42,11 @@ public class Order {
         this.id = id;
     }
 
-    public Instant getmoment() {
+    public Instant getMoment() {
         return moment;
     }
 
-    public void setmoment(Instant moment) {
+    public void setMoment(Instant moment) {
         this.moment = moment;
     }
 
@@ -50,5 +56,21 @@ public class Order {
 
     public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }
