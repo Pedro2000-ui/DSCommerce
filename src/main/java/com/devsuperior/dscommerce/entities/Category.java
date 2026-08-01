526b6061f4c71c26cef20dd50c699fd6f0f1bcd7
Pedro Henrique <pholiveirabrito@hotmail.com>
2026-08-01 13:53:21 -0300

    feat: entities Product, Category, relacionamento muitos-para-muitos e definicições de colunas

---
+++ src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long        id;
+    private String      name;
+
+    /**
+     * Em relacionamentos muitos-para-muitos é comum utilizar Set,
+     * pois ele impede elementos duplicados na coleção, refletindo
+     * a regra de que uma categoria não deve estar associada duas vezes
+     * ao mesmo produto.
+     */
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<Product>();
+
+    public Category() {}
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+}
--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -12,12 +12,14 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String email;
-    private String phone;
-    private LocalDate birthDate;
-    private String password;
+    private Long        id;
+    private String      name;
+
+    @Column(unique = true)
+    private String      email;
+    private String      phone;
+    private LocalDate   birthDate;
+    private String      password;
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
